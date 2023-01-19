316288d8f94a59af6d770cd08a4b782bdc9733ff
Jaco-Miras <58285015+Jaco-Miras@users.noreply.github.com>
2023-01-19 23:06:46 +0800

    final version

--- src/component/Weather.jsx
+++ src/component/Weather.jsx
@@ -142,7 +142,7 @@ const Weather = () => {
           animate ? "animate-shake" : "animate-none"
         } h-16 bg-black/50 w-full max-w-[450px] rounded-full backdrop-blur-[32px] mb-8`}
       >
-        <div className="h-full relative lex items-center justify-between p-2">
+        <div className="h-full relative flex items-center justify-between p-2">
           <input
             onChange={(e) => handleInput(e)}
             className="flex-1 bg-transparent outline-none placeholder:text-white text-white text-[15px] font-light pl-6 h-full"
