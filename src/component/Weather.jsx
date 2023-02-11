b9fea40d4c9cd39040f60d401bc2df0235d72f7a
Jaco-Miras <58285015+Jaco-Miras@users.noreply.github.com>
2023-02-11 11:29:14 +0800

    style adjustments

--- src/component/Weather.jsx
+++ src/component/Weather.jsx
@@ -133,16 +133,16 @@ const Weather = () => {
   const date = new Date();
 
   return (
-    <div className="w-full h-screen bg-gradientBg bg-no-repeat bg-cover bg-center flex flex-col items-center justify-center px-4 lg:px-0">
+    <div className="w-full h-screen bg-gradientBg bg-no-repeat bg-cover bg-center flex flex-col items-center justify-center">
       {errorMsg && (
-        <div className="w-full max-w-[100vw] lg:max-w-[450px] bg-[#25C3FF] text-white top-2 lg:top-10 rounded-full p-4 capitalize my-2">{`${errorMsg.response.data.message}`}</div>
+        <div className="w-[460px] text-center max-w-[100vw] md:-w-[450px] bg-[#25C3FF] text-white top-2 lg:top-10 rounded-full p-4 capitalize my-2">{`${errorMsg.response.data.message}`}</div>
       )}
       <form
         className={`${
           animate ? "animate-shake" : "animate-none"
-        } h-16 bg-black/50 w-full max-w-[450px] rounded-full backdrop-blur-[32px] mb-8`}
+        } h-16 bg-black/50 w-full max-w-[400px] rounded-full backdrop-blur-[32px] mb-2 md:max-w-[460px]`}
       >
-        <div className="h-full relative flex items-center justify-between p-2">
+        <div className="h-full relative w-full flex items-center justify-between p-2">
           <input
             onChange={(e) => handleInput(e)}
             className="flex-1 bg-transparent outline-none placeholder:text-white text-white text-[15px] font-light pl-6 h-full"
@@ -158,7 +158,7 @@ const Weather = () => {
         </div>
       </form>
       {/* card */}
-      <div className="w-full max-w-[480px] bg-black/40 min-h-[590px] text-white rounded-[30px] backdrop-blur-[32px] py-12 px-6">
+      <div className="w-full max-w-[440px] md:max-w-[480px] bg-black/40 max-h-[440px] md:max-h-[460px] text-white rounded-[30px] backdrop-blur-[32px] py-10 px-4 md:py-12 md:px-6">
         {loading ? (
           <div className="w-full h-full flex justify-center items-center">
             <ImSpinner8 className="text-white text-5xl animate-spin" />
@@ -166,12 +166,12 @@ const Weather = () => {
         ) : (
           <div>
             {/* card top */}
-            <div className="flex items-center gap-x-5">
+            <div className="flex items-center gap-x-5 md:px-2 px-6 ">
               {/* icon */}
-              <div className="text-[90px]">{icon}</div>
+              <div className="text-[70px] md:text-[90px]">{icon}</div>
               {/* country name */}
               <div>
-                <div className="text-2xl font-semibold">
+                <div className="text-xl font-semibold md:text-2xl">
                   {data.name}, {data.sys.country}
                 </div>
                 {/* date */}
@@ -182,24 +182,24 @@ const Weather = () => {
               </div>
             </div>
             {/* card body */}
-            <div className="my-20">
+            <div className="my-3 md:my-4">
               <div className="flex justify-center items-center">
                 {/* temp */}
-                <div className="text-[145px] leading-none font-light">
+                <div className="text-[125px] md:text-[150px] leading-none font-light">
                   {parseInt(data.main.temp)}
                 </div>
                 {/* celsius */}
-                <div className="text-5xl">
+                <div className="text-5xl md:text-7xl ">
                   <TbTemperatureCelsius />
                 </div>
               </div>
               {/* weather description */}
-              <div className="capitalize text-center">
+              <div className="capitalize text-center md:text-xl">
                 {data.weather[0].description}
               </div>
             </div>
             {/* card bottom */}
-            <div className="max-w-[380px] mx-auto flex flex-col gap-y-6">
+            <div className="max-w-[380px] md:max-w-[420px] mx-auto flex flex-col gap-y-6 my-2 md:my-5">
               <div className="flex justify-between">
                 <div className="flex items-center gap-x-2">
                   {/* icon */}
